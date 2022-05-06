b35b1fe831ef7c9dbb3c4269e1b9b94dae453766
3arpbl3y <3arpbl3y@gmail.com>
2022-05-06 22:22:49 +0500

    items add in itemList fn

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const items = [];
 
 function addItem(e) {
   e.preventDefault();
-
   const text = e.target.item.value;
   const item = {
     text: text,
     checked: false,
   };
   items.push(item);
+  displayItems(items, itemList);
   this.reset();
-  console.log(items);
+}
+
+function displayItems(ingridients, ingridientsList) {
+  console.log(ingridients, ingridientsList);
+  ingridientsList.innerHTML = ingridients.map((ingridient, index) => {
+    return `<ul>${ingridient.text}</ul>`;
+  });
 }
 
 addItemsForm.addEventListener("submit", addItem);
